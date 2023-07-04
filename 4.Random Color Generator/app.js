75932028213e8edbc489c820e26044a0cc9a67ba
Simranjit911 <simranjit94938@gmail.com>
2023-07-04 20:49:07 +0530

    drawing app

--- 4.Random Color Generator/app.js
+++ 4.Random Color Generator/app.js
@@ -4,6 +4,7 @@ const getcolor = ()=>{
     document.body.style.backgroundColor=hexcode
     document.getElementById('btn').style.backgroundColor=hexcode
     document.getElementById('colorcode').textContent=hexcode
+    // Copy TO Clipboard
     navigator.clipboard.writeText(hexcode)
 }
 document.getElementById('btn').addEventListener('click',getcolor)
